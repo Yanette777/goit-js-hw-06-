a60ed4b40caec33a75421d693c15002fa8522996
Yana Ivanova <yaxo4ka1988@gmail.com>
2022-03-30 20:46:38 +0300

    last changes

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const buttonChangeColorEl = document.querySelector(".change-color");
+const bgColor = document.querySelector(".color");
+
+buttonChangeColorEl.addEventListener("click", () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  bgColor.textContent = bodyEl.style.backgroundColor;
+});
