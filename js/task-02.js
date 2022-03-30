9ffe56818114cf170acfe5dba22e8675cd94cfe0
Yana Ivanova <yaxo4ka1988@gmail.com>
2022-03-30 20:01:23 +0300

    1

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const foodItemsList = document.querySelector(`#ingredients`);
+
+const addListItems = (array) => {
+  const listArray = array.map((item) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = item;
+    return listItem;
+  });
+  foodItemsList.append(...listArray);
+};
+
+addListItems(ingredients);
